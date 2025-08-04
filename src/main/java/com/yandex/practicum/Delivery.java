81b658c4b3c53f9103d308ae6e786aa8e3a6b1d4
Дмитрий Шилин <snayp@yandex.ru>
2025-08-04 13:03:24 +0300

    add Delivery class

---
+++ src/main/java/com/yandex/practicum/Delivery.java
@@ -0,0 +1,59 @@
+package com.yandex.practicum;
+
+public class Delivery {
+
+    public static int calculateCost(int distance, String dimensions, boolean isFragile, String workload) {
+        // Проверка на невозможность доставки хрупкого груза
+        if (isFragile && distance > 30) {
+            throw new IllegalArgumentException("Хрупкие грузы нельзя возить на расстояние более 30 км");
+        }
+
+        int cost = 0;
+
+        // Расчет стоимости по расстоянию
+        if (distance > 30) {
+            cost += 300;
+        } else if (distance > 10) {
+            cost += 200;
+        } else if (distance > 2) {
+            cost += 100;
+        } else {
+            cost += 50;
+        }
+
+        // Расчет стоимости по габаритам
+        if (dimensions.equalsIgnoreCase("большие")) {
+            cost += 200;
+        } else if (dimensions.equalsIgnoreCase("маленькие")) {
+            cost += 100;
+        }
+
+        // Добавление стоимости за хрупкость
+        if (isFragile) {
+            cost += 300;
+        }
+
+        // Применение коэффициента загруженности
+        double workloadCoefficient = 1.0;
+        switch (workload.toLowerCase()) {
+            case "очень высокая":
+                workloadCoefficient = 1.6;
+                break;
+            case "высокая":
+                workloadCoefficient = 1.4;
+                break;
+            case "повышенная":
+                workloadCoefficient = 1.2;
+                break;
+        }
+
+        cost = (int) Math.ceil(cost * workloadCoefficient);
+
+        // Проверка на минимальную стоимость
+        if (cost < 400) {
+            cost = 400;
+        }
+
+        return cost;
+    }
+}
--- src/main/java/com/yandex/practicum/Main.java
+++ src/main/java/com/yandex/practicum/Main.java
@@ -2,6 +2,8 @@ package com.yandex.practicum;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // System.out.println("Hello world!");
+        System.out.println(Delivery.calculateCost(25, "большие", true, "высокая"));
     }
+
 }
